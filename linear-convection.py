658c931bac7ba61632db663bc0447e1d3ff85139
coli <colingraham2@gmail.com>
2016-12-11 14:29:20 -0600

    update of linear-convection file

--- linear-convection.py
+++ linear-convection.py
@@ -2,16 +2,14 @@
 """
 cfd-with-python
 
+linear convection
+du/dt + c*du/dx = 0
+
 """
 
-# Remember: comments in python are denoted by the pound sign
-import numpy as np 
+import numpy as np
 import matplotlib.pyplot as plt
 import time, sys  
-
-
-#this makes matplotlib plots appear in the notebook (instead of a separate window)
-#%matplotlib inline
 
 
 nx = 41    # try changing this number from 41 to 81 and Run All ... what happens?
@@ -26,18 +24,22 @@ u[int(.5 / dx):int(1 / dx + 1)] = 2  #setting u = 2 between 0.5 and 1 as per our
 
 x=np.linspace(0,2,nx)
 
-plt.plot( x, u)
+plt.plot( x, u,label='t=0')
+
+
+un = np.ones(nx)  # initialize a temporary array
+
+for n in range(nt):  # loop for values of n from 0 to nt, so it will run nt times
+    un = u.copy()  ##copy the existing values of u into un
+    # for i in range(1, nx):  ## you can try commenting this line and...
+    for i in range(nx): ## ... uncommenting this line and see what happens!
+        u[i] = un[i] - c * dt / dx * (un[i] - un[i - 1])
+
+
+x=np.linspace(0,2,nx)
+plt.plot( x, u,label='t='+str(nt*dt))
+plt.xlabel('x',fontsize = 20)
+plt.ylabel('u',fontsize = 20)
+plt.legend()
+plt.title('Wave with c='+str(c))
 plt.show()
-
-# un = np.ones(nx)  # initialize a temporary array
-#
-# for n in range(nt):  # loop for values of n from 0 to nt, so it will run nt times
-#     un = u.copy()  ##copy the existing values of u into un
-#     for i in range(1, nx):  ## you can try commenting this line and...
-#         # for i in range(nx): ## ... uncommenting this line and see what happens!
-#         u[i] = un[i] - c * dt / dx * (un[i] - un[i - 1])
-#
-#
-#
-# plt.plot(np.linspace(0, 2, nx), u);
-
